0fcf9928714df7fa717cb4b9756a6928cf18beec
tillettr2025-cpu <tillettr2025@eden.edu.bz>
2026-07-31 16:18:43 -0600

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "️️️️️️️️️️️️️️The best server", // required by Discord API, not shown in the client
         state: "SweepSMP",     // this is what people actually see
-        type: 3,               // Custom
+        type: 0,               // Custom
       },
     ],
   },
